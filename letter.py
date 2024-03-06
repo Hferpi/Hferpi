6ecb425c07a8e5710c7fd9734324be8be8ed278b
Hector <161331335+Hferpi@users.noreply.github.com>
2024-03-06 20:03:03 +0100

    learning sh*_*ts
    in this field u see my starts in code

---
+++ letter.py
@@ -0,0 +1,7 @@
+#i try to code my letter it s a task of codedex.io page
+
+print ('06 of march of 2024') #Today's date.
+print ('I m very happy, so i think i m improving my english and in the same time i learning code') #How you are feeling right now.
+print ('I want get a better job and make more money, in addition i like do something for the world') #What you want to accomplish by learning to code.
+print ('Ey bro!!! Congratulations u are succedsful') #A little message to your older, wiser, and programmer self.
+print ('🚀 🫡  ') #Your favorite emoji to spice things up!
